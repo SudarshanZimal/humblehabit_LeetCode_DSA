e2134f78d99e8d6372aef7e520a291b9b3e47881
Sudarshan Zimal <98067580+SudarshanZimal@users.noreply.github.com>
2023-04-27 19:19:35 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (12.48%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int bulbSwitch(int n) {
+//         if(  n == 0 )return 0;
+//         if( n==1 )return 1;
+//            if( n==2 )return 1;
+//        if( n==3 )return 1;
+//         ArrayList<Integer> res =  new ArrayList<>();
+//         for( int i = 0 ; i < n ; i++){
+//             res.add(i);
+//         }
+//         for( int i = 0 ; i < res.size() ; i++){
+//          if( i != 0 ){
+//              res.remove(i);
+//          }
+          
+            
+//         }
+//         return res.size() ; 
+        return ( int ) Math.sqrt(n);
+    }
+}
